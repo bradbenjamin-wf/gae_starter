857405e739f2108f43b5dfd0af8e2b5b25d2c854
Sage.Smith <sage.smith@workiva.com>
2016-06-10 20:15:48 -0600

    Flake8, requirements, 403 error added

--- payments.py
+++ payments.py
@@ -1,30 +1,34 @@
-import stripe, logging
+import stripe
+import logging
 from bottle import request
 
 
-def stripe_pay(amount, token, user):
+def stripe_pay(amount, user):
     """
-    simplistic Stripe payment handler, returns a tuple 
+    simplistic Stripe payment handler, returns a tuple
     1. successful charge information
     2. message
     """
-    # Set your secret key: remember to change this to your live secret key in production
+    # Set your secret key: remember to change this to your
+    # live secret key in production
     # See your keys here https://manage.stripe.com/account
     stripe.api_key = "sk_test_PQmCHxMcjCGfzbFstIuIYRkr"
-    
+
     # Get the credit card details submitted by the form
     token = request.POST['stripeToken']
-    success = False
     # Create the charge on Stripe's servers - this will charge the user's card
     try:
         charge = stripe.Charge.create(
-            amount=amount, # amount in cents, again
+            amount=amount,  # amount in cents, again
             currency="usd",
             card=token,
-            description="Site Notifier upgrade for user_id {}, email {}".format(user.user_id(), user.email())
+            description="Site Notifier upgrade for user_id {}, "
+                        "email {}".format(user.user_id(), user.email())
         )
         logging.info('payment id: {}'.format(charge.id))
         return charge, "Payment successful"
-    except stripe.CardError, e:
-        return None, "The card was declined, please check card information and try again"
- 
+    except stripe.CardError:
+        return (
+            None,
+            "The card was declined, please check card information and try again"
+        )
